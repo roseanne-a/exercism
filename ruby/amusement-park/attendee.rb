f1ca516ab669410aa74c2a7c8658185bae136910
roseanne-a <roseanne.arcebido@gmail.com>
2023-01-31 07:11:20 -0500

    first commit

---
+++ ruby/amusement-park/attendee.rb
@@ -0,0 +1,24 @@
+class Attendee
+  def initialize(height)
+    @height = height
+    @pass_id
+
+  end
+
+  def height
+    @height
+  end
+
+  def pass_id
+    @pass_id
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+    @pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+end
